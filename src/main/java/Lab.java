7d24536c1bafcf0783746937729ab43587625529
Nya Currie <nya1998@gmail.com>
2023-02-14 23:23:14 +0000

    Hello/Polly

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -4,7 +4,7 @@ public class Lab {
 
     /**
      * This method should return whatever String was passed into it by whatever invoked it (in this case, the test),
-     * instead of returning "return text please!"
+     * instead of returning "return text please!
      *
      * for example:
      * String str = copy("hello");
